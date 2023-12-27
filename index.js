f19047ff08fcdd8db7471e4c041e94bfcc25c51c
Jerimiah Welch <welchjerimiah@gmail.com>
2023-12-26 20:01:28 -0800

    add 3 LEDs

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Gpio = require('onoff').Gpio
 const led = new Gpio(17, 'out')
+const led2 = new Gpio(27, 'out')
+const led3 = new Gpio(22, 'out')
 
-console.log('hi');
+console.log("LEO's led server");
 
 os = require('os');
 const http = require('http');
@@ -10,6 +12,13 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   if (req.url === '/?led=on') led.write(1);
   if (req.url === '/?led=off') led.write(0);
+  
+  if (req.url === '/?led2=on') led2.write(1);
+  if (req.url === '/?led2=off') led2.write(0);
+  
+  if (req.url === '/?led3=on') led3.write(1);
+  if (req.url === '/?led3=off') led3.write(0);
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   
@@ -22,15 +31,34 @@ const server = http.createServer((req, res) => {
   <title>LED Demo</title>
 </head>
 <body>
+<style>
+  button, input {
+    height: 3rem;
+    min-width: 8rem;
+    margin: 0.5rem;
+  }
+</style>
   <h2>A simple LED control server</h2>
-  <p>LED state: <a href="?led=on"> <button>ON</button></a>
-<a href="?led=off"><button>OFF</button></a></p>
+  <p></p>
+  <p>
+    LED 1
+    <a href="?led=on"> <button>ON</button></a>
+    <a href="?led=off"><button>OFF</button></a>
+  </p>
+  <p>
+    LED 2
+    <a href="?led2=on"> <button>ON</button></a>
+    <a href="?led2=off"><button>OFF</button></a>
+  </p>
+  <p>
+    LED 3
+    <a href="?led3=on"> <button>ON</button></a>
+    <a href="?led3=off"><button>OFF</button></a>
+  </p>
 </body>
 </html>  
   `)
   
-  
-    
   res.end();
 });
  
